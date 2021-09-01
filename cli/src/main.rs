5f546fabc3379f4569fe638ef61d489dec91a412
soeur <juzi201314@gmail.com>
2021-09-01 19:34:15 +0800

    删除多分配器支持, 删除tests里不需要的东西

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,7 @@
+#[cfg(feature = "snmalloc-rs")]
+#[global_allocator]
+static GLOBAL: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
+
 #[tokio::main(flavor = "multi_thread")]
 async fn main() {
     core::run().await.unwrap();
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -4,34 +4,7 @@ mod prof;
 use once_cell::sync::Lazy;
 
 use config::CONFIG;
-use database::Database;
 use std::sync::Arc;
-
-#[cfg(any(
-    all(feature = "use_jemalloc", feature = "use_mimalloc",),
-    all(feature = "use_jemalloc", feature = "use_snmalloc",),
-    all(feature = "use_mimalloc", feature = "use_snmalloc",),
-))]
-compile_error!(
-    "Only one memory allocator can be used at the same time!"
-);
-
-#[cfg(all(target_env = "msvc", feature = "use_jemalloc"))]
-compile_error!(
-    "jemalloc does not support msvc targets. Please refer to https://github.com/gnzlbg/jemallocator#platform-support for available platforms."
-);
-
-#[cfg(all(not(target_env = "msvc"), feature = "use_jemalloc"))]
-#[global_allocator]
-static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
-
-#[cfg(feature = "use_mimalloc")]
-#[global_allocator]
-static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
-
-#[cfg(feature = "use_snmalloc")]
-#[global_allocator]
-static GLOBAL: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
 
 pub async fn init() {
     Lazy::force(&CONFIG);
@@ -42,7 +15,9 @@ pub async fn run() -> anyhow::Result<()> {
     let guard = prof::start();
 
     logger::init();
-    let db = Arc::new(Database::new().await?);
+
+    let conf = config::get_config();
+    let db = Arc::new(database::new(conf.database()).await?);
 
     http::generate_password_if_no_exists()?;
     http::run_http_server(Arc::clone(&db)).await?;
