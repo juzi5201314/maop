167c86e4c6ff6b32b2ad7f09a8e01c2808459e12
soeur <juzi201314@gmail.com>
2021-09-07 22:06:14 +0800

    完成logout

--- http/src/lib.rs
+++ http/src/lib.rs
@@ -4,17 +4,18 @@ use std::net::{IpAddr, SocketAddr};
 use std::str::FromStr;
 
 use async_session::MemoryStore;
-use axum::handler::get;
+use axum::handler::{get, post};
 use axum::{AddExtensionLayer, Router};
 use tokio::net::TcpListener;
 
 use ::error::Error;
 
 use crate::routes::index::index;
+use crate::routes::login::{login, logout};
 
-mod routes;
 mod auth;
 mod error;
+mod routes;
 
 pub async fn run_http_server() -> Result<(), Error> {
     let conf_guard = config::get_config();
@@ -22,27 +23,31 @@ pub async fn run_http_server() -> Result<(), Error> {
 
     let axum_app = Router::new()
         .route("/", get(index))
+        .route("/auth", post(login).delete(logout))
         .layer(AddExtensionLayer::new(config.clone()))
         // todo: 持久化
         .layer(AddExtensionLayer::new(MemoryStore::new()));
 
     if config.r#type() == "unix" {
         #[cfg(target_os = "unix")]
-            {
-                use hyperlocal::UnixServerExt;
-                hyper::Server::bind_unix(config.bind())?
-                    .serve(axum_app.into_make_service()).await
-            }
+        {
+            use hyperlocal::UnixServerExt;
+            hyper::Server::bind_unix(config.bind())?
+                .serve(axum_app.into_make_service())
+                .await
+        }
         #[cfg(not(target_os = "unix"))]
-            {
-                panic!("Unsupported. You cannot use unix sockets on non-Unix systems.")
-            }
+        {
+            panic!("Unsupported. You cannot use unix sockets on non-Unix systems.")
+        }
     } else {
         let addr = SocketAddr::new(
             IpAddr::from_str(config.bind())?,
             *config.port(),
         );
-        hyper::Server::bind(&addr).serve(axum_app.into_make_service()).await
+        hyper::Server::bind(&addr)
+            .serve(axum_app.into_make_service())
+            .await
     };
 
     Ok(())
--- http/src/routes/login.rs
+++ http/src/routes/login.rs
@@ -1,12 +1,14 @@
 use crate::auth::LoginStatus;
 use crate::error::HttpServerError;
 use async_session::{MemoryStore, Session, SessionStore};
+use axum::body::Body;
 use axum::extract::Extension;
 use axum::response::IntoResponse;
 use axum::Json;
 use chrono::Duration;
 use config::HttpConfig;
-use hyper::{Response, StatusCode};
+use hyper::header::COOKIE;
+use hyper::{Request, Response, StatusCode};
 use serde::Deserialize;
 use std::ops::Add;
 use utils::password_hash::password_verify;
@@ -44,4 +46,38 @@ pub async fn login(
 #[derive(Deserialize)]
 pub struct LoginData {
     password: String,
+}
+
+pub async fn logout(
+    login_status: LoginStatus,
+    Extension(store): Extension<MemoryStore>,
+    req: Request<Body>,
+) -> Result<(StatusCode, &'static str), (StatusCode, String)> {
+    Ok(match login_status {
+        LoginStatus::Guest => {
+            (StatusCode::FORBIDDEN, "You are not logged in")
+        }
+        LoginStatus::Logged => {
+            if let Some(cookie) = req
+                .headers()
+                .get(COOKIE)
+                .and_then(|value| value.to_str().ok())
+                .map(|value| value.to_string())
+            {
+                if let Some(session) = store
+                    .load_session(cookie)
+                    .await
+                    .server_error("load session failed")?
+                    .map(|s| s.validate())
+                    .flatten()
+                {
+                    store
+                        .destroy_session(session)
+                        .await
+                        .server_error("destroy session failed")?;
+                }
+            }
+            (StatusCode::RESET_CONTENT, "")
+        }
+    })
 }
