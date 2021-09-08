a7d124433fe16bcc98723e7da5d607dca28fb56b
soeur <juzi201314@gmail.com>
2021-09-08 13:10:52 +0800

    fix bug

--- config/src/models/http.rs
+++ config/src/models/http.rs
@@ -2,6 +2,7 @@ crate::gen_config!(HttpConfig, {
     bind: String,
     port: u16,
     r#type: String,
+    #[serde(default)]
     #[serde(deserialize_with = "utils::password_hash::password_hash")]
     password: Option<String>
 });
