92190fe525b801ee48640afe648072b1293833ba
juzi5201314 <juzi201314@gmail.com>
2021-11-05 21:18:47 +0800

    utils(encode_minimal_w to encode_attribute_w; 防止xss漏洞)

--- utils/src/markdown.rs
+++ utils/src/markdown.rs
@@ -1,5 +1,5 @@
 use anyhow::Context;
-use htmlescape::encode_minimal_w;
+use htmlescape::encode_attribute_w;
 use pulldown_cmark::{html, Options, Parser};
 
 pub fn render(s: &str) -> anyhow::Result<String> {
@@ -12,7 +12,7 @@ pub fn render(s: &str) -> anyhow::Result<String> {
 
 pub fn render_safe(s: &str) -> anyhow::Result<String> {
     let mut output = Vec::with_capacity(s.len());
-    encode_minimal_w(s, &mut output).context("escape html")?;
+    encode_attribute_w(s, &mut output).context("escape html")?;
     let parser = Parser::new_ext(
         unsafe { std::str::from_utf8_unchecked(&output) },
         Options::all(),
